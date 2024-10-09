44b56d62083e7840509f9d8f5a3c824022f6c444
Angelo Arcillas <angeloarcillas64@gmail.com>
2024-10-09 18:50:09 +0800

    Update Cacheable.php

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -12,7 +12,7 @@ trait Cacheable
      */
     public function getCacheKey(): string
     {
-        return sprintf('%s-%s-%s', get_class($this), $this->id, $this->updated_at->timestamp);
+        return sprintf('%s-%s-%s', get_class($this), $this->getKey(), $this->updated_at->timestamp);
     }
 
     // public function hasCache(): string
