ea7b90d490a41c1845b2b1a4f7ad26b6cda0fbee
Angelo Arcillas <angeloarcillas64@gmail.com>
2024-10-09 18:24:05 +0800

    Add Cacheable.php

---
+++ src/Cacheable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zerexei\LaravelModelHelper;
+
+/**
+ * 
+ */
+trait Cacheable
+{
+    /**
+     * 
+     */
+    public function getCacheKey(): string
+    {
+        return sprintf('%s-%s-%s', get_class($this), $this->id, $this->updated_at->timestamp);
+    }
+
+    // public function hasCache(): string
+    // {
+    //     return \Illuminate\Support\Facades\Cache::has($this->getCacheKey());
+    // }
+
+    // public function getCache(?callable $default): mixed
+    // {
+    //     return \Illuminate\Support\Facades\Cache::get($this->getCacheKey(), $default);
+    // }
+
+    // public function putCache(mixed $value, $seconds = 10): mixed
+    // {
+    //     return \Illuminate\Support\Facades\Cache::put($this->getCacheKey(), $value, $seconds);
+    // }
+
+    // public function rememberCache($seconds, callable $callable): mixed
+    // {
+    //     return \Illuminate\Support\Facades\Cache::remember($this->getCacheKey(), $seconds, $callable);
+    // }
+
+    // public function rememberForeverCache(callable $callable): mixed
+    // {
+    //     return \Illuminate\Support\Facades\Cache::rememberForever($this->getCacheKey(), $callable);
+    // }
+}
